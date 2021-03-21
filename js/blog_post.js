722ab8d7404ff67a0392d7fee857c17b5130ea91
Viola Josehine Hestbech <viol1397@stud.kea.dk>
2021-03-21 23:14:30 +0100

    I'm amazing (everything is working now)

--- js/blog_post.js
+++ js/blog_post.js
@@ -1,6 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 //in the url grab id and store them under id
 const id = urlParams.get("id");
+const topic = urlParams.get("topic");
+const length = urlParams.get("length");
 console.log(id);
 
 const url = "https://kea21spring-0a0d.restdb.io/rest/test/" + id;
@@ -17,7 +19,7 @@ fetch(url, options)
 function showPost(post) {
   console.log(post);
 
-  document.querySelector("#genre").textContent = post.genre;
+  document.querySelector("#genre_text").textContent = post.topic;
   document.querySelector("#title").textContent = post.title;
   document.querySelector("#alternative_headline").textContent =
     post.alternate_headline;
@@ -30,4 +32,56 @@ function showPost(post) {
   document.querySelector("#quote").textContent = post.quote;
   document.querySelector("#quote_author").textContent = post.quote_author;
   document.querySelector("#body2").textContent = post.body2;
+
+  const other_url =
+    "https://kea21spring-0a0d.restdb.io/rest/test?max=" +
+    post.length +
+    "&?q={topic:" +
+    post.topic +
+    "}";
+  console.log(other_url);
+  console.log(post.topic);
+  console.log(post.length);
+
+  const other_options = {
+    headers: {
+      "x-apikey": "60534ad0ff8b0c1fbbc28be2",
+    },
+  };
+
+  fetch(other_url, other_options)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (otherdata) {
+      handleOtherPosts(otherdata);
+      console.log(otherdata);
+    });
+
+  function handleOtherPosts(otherdata) {
+    otherdata.forEach(showOtherPosts);
+    console.log(otherdata);
+  }
+
+  function showOtherPosts(otherpost) {
+    console.log(otherpost);
+    //grab template
+    const template = document.querySelector(".post_template").content;
+    //clone it
+    const copy = template.cloneNode(true);
+    //change content
+
+    copy.querySelector("a").href = "blog_post.html?id=" + otherpost._id;
+    copy.querySelector(".other_post_title").textContent = otherpost.title;
+    copy.querySelector(".other_post_date").textContent =
+      otherpost.date_published;
+    copy.querySelector(".other_post_image").src =
+      "https://kea21spring-0a0d.restdb.io/media/" + otherpost.image;
+    copy.querySelector(".other_post_text").textContent = otherpost.intro;
+
+    //grab parent
+    const parent = document.querySelector("#other_posts");
+    //append child
+    parent.appendChild(copy);
+  }
 }
