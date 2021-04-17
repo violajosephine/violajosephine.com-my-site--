08ed5b9171e1584aac5a4beeaf0c73fcde922ad7
Viola Josehine Hestbech <viol1397@stud.kea.dk>
2021-04-17 20:59:34 +0200

    Don't remember

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,6 @@
 const url =
-  "https://kea21spring-0a0d.restdb.io/rest/test?q={}&sort=_created&dir=-1";
+  "https://kea21spring-0a0d.restdb.io/rest/test?q={}&sort=_created&dir=-1&?fetchchildren=true";
+
 const options = {
   headers: {
     "x-apikey": "60534ad0ff8b0c1fbbc28be2",
@@ -31,8 +32,47 @@ function showContent(post) {
     copy.querySelector("a").href = "blog_post.html?id=" + post._id;
     copy.querySelector(".post_title").textContent = post.title;
     copy.querySelector(".post_date").textContent = post.date_published;
+
+    // const image_url =
+    //   "https://kea21spring-0a0d.restdb.io/media/" + post.image + "/meta";
+    // const image_options = {
+    //   headers: {
+    //     "x-apikey": "60534ad0ff8b0c1fbbc28be2",
+    //   },
+    // };
+
+    // fetch(image_url, image_options)
+    //   .then(function (res) {
+    //     return res.json();
+    //   })
+    //   .then(function (images) {
+    //     handleImages(images);
+    //     console.log(images);
+    //   });
+    // function handleImages(data) {
+    //   data.forEach(showImages);
+    //   console.log(images);
+    // }
+    // function showImages(images) {
+    //   const template = document.querySelector(".post_template").content;
+    //   //clone it
+    //   const copy = template.cloneNode(true);
+    //   copy.querySelector(".post_image").src =
+    //     "https://kea21spring-0a0d.restdb.io/media/" +
+    //     post.image +
+    //     "?key=" +
+    //     images.access_key;
+    //   console(images.acess_key);
+
+    //   const parent = document.querySelector("#posts");
+    //   //append child
+    //   parent.appendChild(copy);
+    // }
+
+    //TEMPORARY IMAGE FETCH
     copy.querySelector(".post_image").src =
       "https://kea21spring-0a0d.restdb.io/media/" + post.image;
+
     // const noimage = copy.querySelector(".post_image");
     // if (noimage.src == "https://kea21spring-0a0d.restdb.io/media/") {
     //   console.log(noimage.src);
@@ -53,6 +93,20 @@ function showContent(post) {
 
     copy.querySelector(".thought_text").innerHTML = post.intro;
     copy.querySelector(".date_thought").textContent = post.date;
+
+    //grab parent
+    const parent = document.querySelector("#posts");
+    //append child
+    parent.appendChild(copy);
+  } else if (post.type == "quote") {
+    //grab template
+    const template = document.querySelector(".quote_template").content;
+    //clone it
+    const copy = template.cloneNode(true);
+    //change content
+
+    copy.querySelector(".quote_text").innerHTML = post.intro;
+    copy.querySelector(".date_quote").textContent = post.date;
 
     //grab parent
     const parent = document.querySelector("#posts");
